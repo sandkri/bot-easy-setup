a28df14b4f68f663246f1c25f0f93ec9b56b1b2b
Xynterical <xynterical@gmail.com>
2025-03-11 19:48:55 +0100

    Fixed bugs

--- src/commands/management/set.js
+++ src/commands/management/set.js
@@ -14,6 +14,7 @@ export default {
     )
     .setDefaultMemberPermissions(PermissionsBitField.Flags.ManageChannels),
     
+    
   async execute(interaction) {
     const data = readJSON('servers.json');
     console.log(data);
