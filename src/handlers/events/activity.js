8c309c962ab249da6c8b5b826b95299c5a930e40
Xynterical <xynterical@gmail.com>
2025-03-11 19:39:38 +0100

    Github Webhook

--- src/handlers/events/activity.js
+++ src/handlers/events/activity.js
@@ -1,5 +1,6 @@
 import { ActivityType } from 'discord.js';
 
+
 export default (client) => {
     client.once('ready', () => {
         client.user.setActivity(`${client.guilds.cache.size} servers`, { type: ActivityType.Watching });
